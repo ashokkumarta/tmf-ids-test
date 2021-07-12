abe89322040ffffec680ba7a04cde2461c4f9674
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-12 11:38:13 +0530

    test apps and connectors

---
+++ i-connectors/mno1.sh
@@ -0,0 +1,4 @@
+docker-compose -f csp-mno-1.yaml stop
+docker-compose -f csp-mno-1.yaml rm -f
+#docker-compose -f csp-mno-1.yaml pull   
+docker-compose -f csp-mno-1.yaml up
---
+++ i-connectors/mno2.sh
@@ -0,0 +1,4 @@
+docker-compose -f csp-mno-2.yaml stop
+docker-compose -f csp-mno-2.yaml rm -f
+#docker-compose -f csp-mno-2.yaml pull   
+docker-compose -f csp-mno-2.yaml up
