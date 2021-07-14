30322c577811ae64a42c32b6b501bad22ece08aa
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-14 06:46:52 +0530

    Ranshare configurations

--- connectors/mno1.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f csp-mno-1.yaml stop
-docker-compose -f csp-mno-1.yaml rm -f
-#docker-compose -f csp-mno-1.yaml pull   
-docker-compose -f csp-mno-1.yaml up
--- connectors/mno2.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f csp-mno-2.yaml stop
-docker-compose -f csp-mno-2.yaml rm -f
-#docker-compose -f csp-mno-2.yaml pull   
-docker-compose -f csp-mno-2.yaml up
